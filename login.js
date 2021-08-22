1cb0352b7ca6878bc6e02315073ef01cfb36f21f
Julius van Dis <vandis.j@gmail.com>
2021-08-22 14:05:06 +0200

    Add screenshots

--- login.js
+++ login.js
@@ -16,10 +16,20 @@ const verifyInput = () => {
     return {host, username, password}
 };
 
+const takeScreenshot = async (page, name) => {
+    await page.screenshot({
+        path: "./screenshots/" + name + ".jpg",
+        type: "jpeg",
+        fullPage: true
+    });
+};
+
 (async () => {
-    const  {host, username: expectedUsername, password} =  verifyInput();
+    const {host, username: expectedUsername, password} = verifyInput();
 
     const browser = await puppeteer.launch({
+        // executablePath: '/usr/bin/chromium',
+        defaultViewport: {width: 680, height: 420},
         args: [
             '--no-sandbox',
             '--disable-setuid-sandbox'
@@ -32,6 +42,7 @@ const verifyInput = () => {
     await page.goto(host, {waitUntil: "networkidle2"});
 
     await page.waitForTimeout(2000)
+    await takeScreenshot(page, "01.loaded");
     const firstUserSelector = 'div.qx-bj-wizard-list-item-first';
     const userName = await page.waitForSelector(firstUserSelector)
         .then(x => x.getProperty('innerText').then(x => x.jsonValue()));
@@ -39,17 +50,55 @@ const verifyInput = () => {
 
     if (!userName.endsWith(expectedUsername)) throw "Username is not as expected"
 
-    await page.click(firstUserSelector);
-    console.log(`Clicked user ${userName} element`)
+    //Check if pop-up
+    try {
+
+        await page.waitForSelector('div.qx-android-dialog', {timeout: 500})
+        await takeScreenshot(page, "01x.compatibility-issues");
+        await page.click('div.qx-android-dialog > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div')
+        console.log("Clicked OK to close popup")
+    } catch (e) {
+        console.log("No popup saying element doesn't exist, yay")
+    }
+
+    const mfg = await page.$x(`//div[text()="${expectedUsername}"]`);
+    const prop = await mfg[0].getProperty('innerText');
+    const text = await prop.jsonValue();
+    console.log(text);
+    console.log(text);
+    console.log(text);
+
+    const textCoords = await mfg[0].boundingBox()
+
+    console.log(`Coordinates: ${textCoords}`, textCoords);
+    let mySelector=firstUserSelector;
+    const myObject = await page.$(mySelector)
+    // const coOrdinates = await myObject.boundingBox()
+    const coOrdinates = textCoords
+    console.log(`Coordinates: ${coOrdinates}`, coOrdinates);
+    await page.mouse.click( coOrdinates.x+coOrdinates.width/2 , coOrdinates.y+coOrdinates.height/2 )
+    // await page.click(firstUserSelector);
+    console.log(`Clicked user "${userName}" element`)
 
 
-    await page.waitForSelector(`${firstUserSelector} input`);
-    console.log(`Found input field for user`)
+    try {
+        await page.waitForSelector(`${firstUserSelector} input`);
+        await takeScreenshot(page, "02.user-selection");
+        console.log(`Found input field for user`)
+        // console.log(await page.content());
+    } catch (e) {
+        console.log("couldn't find input")
+        await takeScreenshot(page, "02x.user-selection-failed");
+        console.log(await page.content());
+        throw e
+    }
 
+    await page.waitForTimeout(3000)
     await page.click(`${firstUserSelector} input`);
     await page.keyboard.type(password, {delay: 10});
     console.log(`Filled in password for user`)
 
+    await takeScreenshot(page, "03.passwod-filled");
 
     await page.click('div.qx-bj-wizard-sep-button-last', {delay: 2000});
 
@@ -57,7 +106,11 @@ const verifyInput = () => {
     await page.waitForTimeout(2000)
     await page.waitForSelector('.qx-bj-switching', {timeout: 3000})
 
+    await takeScreenshot(page, "04.logged-in");
+
     const successful = await page.$('.qx-bj-switching');
+
+    await takeScreenshot(page, "05.really-logged-in");
 
     await browser.close();
     if (successful) {
